5c43d84c04fd964e0d90204920afd17d2d077093
ytkim4558 <ytkim4558@naver.com>
2016-09-07 18:07:32 +0900

    bikenavi tmap 현재 위치 표시

--- app/src/main/java/com/nagnek/bikenavi/MainActivity.java
+++ app/src/main/java/com/nagnek/bikenavi/MainActivity.java
@@ -1,13 +1,68 @@
 package com.nagnek.bikenavi;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.RelativeLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.skp.Tmap.TMapView;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().permitNetwork().build());
+
+        RelativeLayout relativeLayout = new RelativeLayout(this);
+        TMapView tMapView = new TMapView(this);
+        tMapView.setSKPMapApiKey("d2bc2636-c213-3bad-9058-7d46cf9f8039");
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 6000, 10, this);
+        String locationProvider = locationManager.getBestProvider(new Criteria(), true);
+        Location cur_locatoin = locationManager.getLastKnownLocation(locationProvider);
+        tMapView.setCenterPoint(cur_locatoin.getLongitude(), cur_locatoin.getLatitude());
+
+        relativeLayout.addView(tMapView);
+        setContentView(relativeLayout);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
