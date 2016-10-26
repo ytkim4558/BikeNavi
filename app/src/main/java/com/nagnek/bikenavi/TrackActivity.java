1a66a80eacd3a46847ae7a41fa0113be289a0653
ytkim4558 <ytkim4558@naver.com>
2016-10-26 15:50:26 +0900

    경로 액티비티 생성

---
+++ app/src/main/java/com/nagnek/bikenavi/TrackActivity.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2016. UGIF. All Rights Reserved
+ */
+
+package com.nagnek.bikenavi;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class TrackActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_track);
+    }
+}
