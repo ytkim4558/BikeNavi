87a8375694a93388c0a0aaa06d2cd993a7348952
ytkim4558 <ytkim4558@naver.com>
2016-10-28 19:40:52 +0900

    경로설정화면을 TrackSettingFragment.java 로 분리

--- app/src/main/java/com/nagnek/bikenavi/MainActivity.java
+++ app/src/main/java/com/nagnek/bikenavi/MainActivity.java
@@ -4,7 +4,6 @@
 
 package com.nagnek.bikenavi;
 
-import android.app.ActivityOptions;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -13,13 +12,9 @@ import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
 import android.os.StrictMode;
 import android.support.annotation.Nullable;
-import android.support.design.widget.TabLayout;
-import android.support.design.widget.TextInputLayout;
 import android.support.v4.graphics.drawable.DrawableCompat;
-import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
@@ -47,11 +42,9 @@ import com.kakao.usermgmt.callback.LogoutResponseCallback;
 import com.nagnek.bikenavi.activity.LoginActivity;
 import com.nagnek.bikenavi.app.AppConfig;
 import com.nagnek.bikenavi.customview.ClearableSqliteAutoCompleteTextView;
-import com.nagnek.bikenavi.customview.DelayAutoCompleteTextView;
 import com.nagnek.bikenavi.helper.IPManager;
 import com.nagnek.bikenavi.helper.SQLiteHandler;
 import com.nagnek.bikenavi.helper.SessionManager;
-import com.nagnek.bikenavi.util.NagneUtil;
 
 import org.w3c.dom.Document;
 
@@ -72,7 +65,6 @@ public class MainActivity extends AppCompatActivity implements MyAdapter.ClickLi
     // 첫번째로 네비게이션 드로어 리스트뷰에 타이틀과 아이콘을 선언한다.
     // 이 아이콘과 타이틀들은 배열에 담긴다
 
-    static final int SEARCH_INTEREST_POINT = 1; // 장소 검색 request code
     private static final String TAG = MainActivity.class.getSimpleName();
     final String TITLES[] = {"길찾기", "장소찾기"};
     final int ICONS[] = {R.drawable.ic_directions_black_24dp, R.drawable.places_ic_search};
@@ -83,8 +75,6 @@ public class MainActivity extends AppCompatActivity implements MyAdapter.ClickLi
     MyAdapter mAdapter;
     RecyclerView.LayoutManager mLayoutManager;
     ActionBarDrawerToggle mDrawerToggle;
-    DelayAutoCompleteTextView start_point, dest_point;
-    POI start_poi, dest_poi;
     String[] serverIPs = new String[]{};
     /**
      * AutoCompleteTextView 대신 ClearableSQliteAutoCompleteTextView로 변경했다.
@@ -228,25 +218,12 @@ public class MainActivity extends AppCompatActivity implements MyAdapter.ClickLi
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Get the ViewPager and set it's RecentPOIPagerAdapter so that it can display items
-        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
-        RecentTrackPagerAdapter recentTrackPagerAdapter = new RecentTrackPagerAdapter(getSupportFragmentManager(), MainActivity.this);
-        viewPager.setAdapter(recentTrackPagerAdapter);
-
-        // Give the TabLayout the ViewPager
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
-        tabLayout.setupWithViewPager(viewPager);
-
-        // Iterate over all tabs and set the custom view
-        for (int i = 0; i < tabLayout.getTabCount(); ++i) {
-            TabLayout.Tab tab = tabLayout.getTabAt(i);
-            if (tab != null) {
-                tab.setCustomView(recentTrackPagerAdapter.getTabView(i));
-            } else {
-                Log.d(TAG, "tab이 null이네?");
-            }
-        }
+//
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+//        TrackSettingFragment trackSettingFragment = new TrackSettingFragment();
+//        fragmentTransaction.add(R.id.track_fragment_setting_container, trackSettingFragment);
+//        fragmentTransaction.commit();
 
         long start = System.currentTimeMillis();
         // SqLite database handler 초기화
@@ -367,51 +344,7 @@ public class MainActivity extends AppCompatActivity implements MyAdapter.ClickLi
         Log.d(TAG, "구글맵 로딩 시간 : " + (end - start) / 1000.0);
         start = System.currentTimeMillis();
 
-        final TextInputLayout ti_start = (TextInputLayout) findViewById(R.id.ti_start_point);
-        final TextInputLayout ti_dest = (TextInputLayout) findViewById(R.id.ti_dest_point);
-        /**
-         * 출발지나 도착지 입력창을 클릭하면 검색 액티비티로 넘어간다.
-         */
-        start_point = (DelayAutoCompleteTextView) findViewById(R.id.start_point);
-        start_point.setKeyListener(null);
-        start_point.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Performing stop of activity that is not resumed: {com.nagnek.bikenavi/com.nagnek.bikenavi.MainActivity} 에러 방지
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        Intent intent = new Intent(MainActivity.this, SearchActivity.class);
-                        intent.putExtra(getResources().getString(R.string.name_purpose_search_point), "출발");
-                        intent.putExtra(getResources().getString(R.string.current_point_text_for_transition), start_point.getText().toString());
-                        // 화면전환 애니메이션을 생성한다. 트랜지션 이름은 양쪽 액티비티에 선언되어야한다.
-                        ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this,
-                                start_point, start_point.getTransitionName());
-                        startActivityForResult(intent, SEARCH_INTEREST_POINT, options.toBundle());
-                    }
-                }, 300);
-            }
-        });
 
-        dest_point = (DelayAutoCompleteTextView) findViewById(R.id.dest_point);
-        dest_point.setKeyListener(null);
-        dest_point.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        Intent intent = new Intent(MainActivity.this, SearchActivity.class);
-                        intent.putExtra(getResources().getString(R.string.name_purpose_search_point), "도착");
-                        intent.putExtra(getResources().getString(R.string.current_point_text_for_transition), dest_point.getText().toString());
-                        // 화면전환 애니메이션을 생성한다. 트랜지션 이름은 양쪽 액티비티에 선언되어야한다.
-                        ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this,
-                                dest_point, dest_point.getTransitionName());
-                        startActivityForResult(intent, SEARCH_INTEREST_POINT, options.toBundle());
-                    }
-                }, 300);
-            }
-        });
         end = System.currentTimeMillis();
         Log.d(TAG, "나머지 : " + (end - start) / 1000.0);
 
@@ -538,58 +471,15 @@ public class MainActivity extends AppCompatActivity implements MyAdapter.ClickLi
         super.onPostCreate(savedInstanceState);
 
         // Sync the toggle state after onRestoreInstanceState has occured.
-
-    }
-
-    // 출발지 도착지 설정할때마다 불러옴.
-    // 둘다 설정한 경우는 경로를 탐색하고 한곳만 설정한 경우는 해당 좌표를 표시한다.
-    void reactionSearchResult() {
-        // start 지점과 도착지점 모두 설정되었으면 경로를 찾는다.
-        if (!start_point.getText().toString().equals("") && !dest_point.getText().toString().equals("")) {
-            redirectTrackActivity();
-        }
-    }
-
-    void redirectTrackActivity() {
-        Intent intent = new Intent(MainActivity.this, TrackActivity.class);
-        intent.putExtra(NagneUtil.getStringFromResources(this.getApplicationContext(), R.string.start_point_text_for_transition), start_point.getText().toString());
-        intent.putExtra(NagneUtil.getStringFromResources(this.getApplicationContext(), R.string.dest_point_text_for_transition), dest_point.getText().toString());
-        startActivity(intent);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        Log.d(TAG, "onActivityResult ");
-        if (requestCode == SEARCH_INTEREST_POINT) { // 장소검색 요청한게 돌아온 경우
-            Log.d(TAG, "SEARCH_INTEREST_POINT");
-            if (resultCode == RESULT_OK) {// 장소 검색 결과 리턴
-                String purposePoint = data.getStringExtra(getStringFromResources(R.string.name_purpose_search_point));
-                Log.d(TAG, "장소입력한 곳은? " + purposePoint);
-                String selectPoint = data.getStringExtra(getStringFromResources(R.string.select_poi_name_for_transition));
-                String address = data.getStringExtra(getStringFromResources(R.string.select_poi_address_for_transition));
-                if (purposePoint.equals("출발")) {
-                    start_point.setText(selectPoint);
-                    start_poi = (POI) data.getSerializableExtra(getStringFromResources(R.string.current_point_poi_for_transition));
-                } else if (purposePoint.equals("도착")) {
-                    dest_point.setText(selectPoint);
-                    dest_poi = (POI) data.getSerializableExtra(getStringFromResources(R.string.current_point_poi_for_transition));
-                } else {
-                    Log.d(TAG, "purposePoint에 값이 없나? 아님 이상한가?");
-                }
-                if (start_poi != null && dest_poi != null) {
-                    Thread thread = new Thread(new Runnable() {
-                        @Override
-                        public void run() {
-                            Track track = new Track();
-                            track.start_poi = start_poi;
-                            track.dest_poi = dest_poi;
-                            db.addTrack(track);
-                        }
-                    });
-                    thread.start();
-                }
-                reactionSearchResult();
-            }
+        Log.d(TAG, "onActivityResult : requestcode = " + requestCode);
+        TrackSettingFragment trackSettingFragment = (TrackSettingFragment) getSupportFragmentManager().findFragmentById(R.id.track_fragment_setting_container);
+
+        if (trackSettingFragment != null) {
+            trackSettingFragment.onActivityResult(requestCode, resultCode, data);
         }
     }
 
@@ -618,9 +508,11 @@ public class MainActivity extends AppCompatActivity implements MyAdapter.ClickLi
 
     @Override
     public void onRecentTrackSelected(Track track) {
-        start_point.setText(track.start_poi.name);
-        dest_point.setText(track.dest_poi.name);
-        db.updateLastUsedAtTrack(track);
-        reactionSearchResult();
+        // TODO : tracksettingfragmnet로 보내주기
+        TrackSettingFragment trackSettingFragment = (TrackSettingFragment) getSupportFragmentManager().findFragmentById(R.id.track_fragment_setting_container);
+
+        if (trackSettingFragment != null) {
+            trackSettingFragment.onRecentTrackSelected(track);
+        }
     }
 }
---
+++ app/src/main/java/com/nagnek/bikenavi/TrackSettingFragment.java
@@ -0,0 +1,178 @@
+/*
+ * Copyright (c) 2016. UGIF. All Rights Reserved
+ */
+
+package com.nagnek.bikenavi;
+
+import android.app.Activity;
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.TabLayout;
+import android.support.design.widget.TextInputLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.nagnek.bikenavi.customview.DelayAutoCompleteTextView;
+import com.nagnek.bikenavi.helper.SQLiteHandler;
+import com.nagnek.bikenavi.util.NagneUtil;
+
+public class TrackSettingFragment extends Fragment {
+    static final int SEARCH_INTEREST_POINT = 1; // 장소 검색 request code
+    private static final String TAG = TrackSettingFragment.class.getSimpleName();
+    DelayAutoCompleteTextView start_point, dest_point;
+    POI start_poi, dest_poi;
+    private SQLiteHandler db;   // sqlite
+
+    public TrackSettingFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_track_setting, container, false);
+
+        // Get the ViewPager and set it's RecentPOIPagerAdapter so that it can display items
+        ViewPager viewPager = (ViewPager) rootView.findViewById(R.id.viewPager);
+        RecentTrackPagerAdapter recentTrackPagerAdapter = new RecentTrackPagerAdapter(getActivity().getSupportFragmentManager(), getActivity());
+        viewPager.setAdapter(recentTrackPagerAdapter);
+
+        // Give the TabLayout the ViewPager
+        TabLayout tabLayout = (TabLayout) rootView.findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
+
+        // Iterate over all tabs and set the custom view
+        for (int i = 0; i < tabLayout.getTabCount(); ++i) {
+            TabLayout.Tab tab = tabLayout.getTabAt(i);
+            if (tab != null) {
+                tab.setCustomView(recentTrackPagerAdapter.getTabView(i));
+            } else {
+                Log.d(TAG, "tab이 null이네?");
+            }
+        }
+
+        long start = System.currentTimeMillis();
+        // SqLite database handler 초기화
+        db = SQLiteHandler.getInstance(getActivity().getApplicationContext());
+
+        final TextInputLayout ti_start = (TextInputLayout) rootView.findViewById(R.id.ti_start_point);
+        final TextInputLayout ti_dest = (TextInputLayout) rootView.findViewById(R.id.ti_dest_point);
+        /**
+         * 출발지나 도착지 입력창을 클릭하면 검색 액티비티로 넘어간다.
+         */
+        start_point = (DelayAutoCompleteTextView) rootView.findViewById(R.id.start_point);
+        start_point.setKeyListener(null);
+        start_point.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Performing stop of activity that is not resumed: {com.nagnek.bikenavi/com.nagnek.bikenavi.MainActivity} 에러 방지
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent(getContext(), SearchActivity.class);
+                        intent.putExtra(getResources().getString(R.string.name_purpose_search_point), "출발");
+                        intent.putExtra(getResources().getString(R.string.current_point_text_for_transition), start_point.getText().toString());
+                        // 화면전환 애니메이션을 생성한다. 트랜지션 이름은 양쪽 액티비티에 선언되어야한다.
+                        ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(getActivity(),
+                                start_point, start_point.getTransitionName());
+                        getActivity().startActivityForResult(intent, SEARCH_INTEREST_POINT, options.toBundle());
+                    }
+                }, 300);
+            }
+        });
+
+        dest_point = (DelayAutoCompleteTextView) rootView.findViewById(R.id.dest_point);
+        dest_point.setKeyListener(null);
+        dest_point.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent(getActivity(), SearchActivity.class);
+                        intent.putExtra(getResources().getString(R.string.name_purpose_search_point), "도착");
+                        intent.putExtra(getResources().getString(R.string.current_point_text_for_transition), dest_point.getText().toString());
+                        // 화면전환 애니메이션을 생성한다. 트랜지션 이름은 양쪽 액티비티에 선언되어야한다.
+                        ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(getActivity(),
+                                dest_point, dest_point.getTransitionName());
+                        getActivity().startActivityForResult(intent, SEARCH_INTEREST_POINT, options.toBundle());
+                    }
+                }, 300);
+            }
+        });
+        return rootView;
+    }
+
+    public void onRecentTrackSelected(Track track) {
+        start_point.setText(track.start_poi.name);
+        dest_point.setText(track.dest_poi.name);
+        db.updateLastUsedAtTrack(track);
+        reactionSearchResult();
+    }
+
+    // 출발지 도착지 설정할때마다 불러옴.
+    // 둘다 설정한 경우는 경로를 탐색하고 한곳만 설정한 경우는 해당 좌표를 표시한다.
+    void reactionSearchResult() {
+        // start 지점과 도착지점 모두 설정되었으면 경로를 찾는다.
+        if (!start_point.getText().toString().equals("") && !dest_point.getText().toString().equals("")) {
+            redirectTrackActivity();
+        }
+    }
+
+
+    void redirectTrackActivity() {
+        Intent intent = new Intent(getContext(), TrackActivity.class);
+        intent.putExtra(NagneUtil.getStringFromResources(getActivity().getApplicationContext(), R.string.start_point_text_for_transition), start_point.getText().toString());
+        intent.putExtra(NagneUtil.getStringFromResources(getActivity().getApplicationContext(), R.string.dest_point_text_for_transition), dest_point.getText().toString());
+        startActivity(intent);
+    }
+
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d(TAG, "왓나?");
+        Log.d(TAG, "requestCode = " + requestCode + " But, I want this requestCode: " + SEARCH_INTEREST_POINT);
+        if (requestCode == SEARCH_INTEREST_POINT) { // 장소검색 요청한게 돌아온 경우
+            Log.d(TAG, "SEARCH_INTEREST_POINT");
+            if (resultCode == Activity.RESULT_OK) {// 장소 검색 결과 리턴
+                String purposePoint = data.getStringExtra(NagneUtil.getStringFromResources(getActivity(), R.string.name_purpose_search_point));
+                Log.d(TAG, "장소입력한 곳은? " + purposePoint);
+                String selectPoint = data.getStringExtra(NagneUtil.getStringFromResources(getActivity(), R.string.select_poi_name_for_transition));
+                String address = data.getStringExtra(NagneUtil.getStringFromResources(getActivity(), R.string.select_poi_address_for_transition));
+                if (purposePoint.equals("출발")) {
+                    start_point.setText(selectPoint);
+                    start_poi = (POI) data.getSerializableExtra(NagneUtil.getStringFromResources(getActivity(), R.string.current_point_poi_for_transition));
+                } else if (purposePoint.equals("도착")) {
+                    dest_point.setText(selectPoint);
+                    dest_poi = (POI) data.getSerializableExtra(NagneUtil.getStringFromResources(getActivity(), R.string.current_point_poi_for_transition));
+                } else {
+                    Log.d(TAG, "purposePoint에 값이 없나? 아님 이상한가?");
+                }
+                if (start_poi != null && dest_poi != null) {
+                    Thread thread = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Track track = new Track();
+                            track.start_poi = start_poi;
+                            track.dest_poi = dest_poi;
+                            db.addTrack(track);
+                        }
+                    });
+                    thread.start();
+                }
+                reactionSearchResult();
+            }
+        }
+    }
+}
