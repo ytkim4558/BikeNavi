2ad3b520881d452d3919d8ee4c793e83c5cc31b7
ytkim4558 <ytkim4558@naver.com>
2016-11-15 15:57:37 +0900

    Instant run 비활성화. 이전커밋이 느려져서 다시 카카오톡 모듈 지워봄. 현재는 빠름

--- app/src/main/java/com/nagnek/bikenavi/app/AppController.java
+++ app/src/main/java/com/nagnek/bikenavi/app/AppController.java
@@ -5,7 +5,9 @@
 package com.nagnek.bikenavi.app;
 
 import android.app.Activity;
-import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
+import android.support.multidex.MultiDexApplication;
 import android.text.TextUtils;
 import android.util.Log;
 
@@ -16,9 +18,7 @@ import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.BasicNetwork;
 import com.android.volley.toolbox.DiskBasedCache;
 import com.android.volley.toolbox.HurlStack;
-import com.kakao.auth.KakaoSDK;
-import com.nagnek.bikenavi.helper.SessionManager;
-import com.nagnek.bikenavi.kakao.KakaoSDKAdapter;
+
 
 /**
  * Created by yongtak on 2016-09-27.
@@ -26,51 +26,27 @@ import com.nagnek.bikenavi.kakao.KakaoSDKAdapter;
  * 카카오톡 로그인을 위한 소스추가 : 2016-10-09 참고 : http://uareuni.tistory.com/13
  */
 
-public class AppController extends Application {
+public class AppController extends MultiDexApplication {
 
     /**
      * 자체로그인 위함.
      */
     public static final String TAG = AppController.class.getSimpleName();
-    /**
-     * 카카오톡 로그인 위한 소스추가
-     */
-    private static volatile AppController instance = null;
-    private static volatile Activity currentActivity = null;
     private static volatile AppController mInstance;
-    /**
-     * 크래쉬 방지용 코드 추가
-     * 참조 : http://www.kmshack.kr/2013/03/uncaughtexceptionhandler%EB%A5%BC-%EC%9D%B4%EC%9A%A9%ED%95%9C-%EC%95%B1-%EB%B9%84%EC%A0%95%EC%83%81-%EC%A2%85%EB%A3%8C%EC%8B%9C-log%EC%A0%84%EC%86%A1-%EB%B0%8F-%EC%9E%AC%EC%8B%A4%ED%96%89-%ED%95%98/
-     */
     private RequestQueue mRequestQueue;
-    private SessionManager session; // 로그인했는지 확인용 변수
-
-    /**
-     * 카카오톡
-     *
-     * @param
-     */
-    public static Activity getCurrentActivity() {
-        return currentActivity;
-    }
 
     public static void setCurrentActivity(Activity activity) {
-        AppController.currentActivity = activity;
-    }
-
-    /**
-     * singleton 애플리케이션 객체를 얻는다.
-     *
-     * @return singleton 애플리케이션 객체
-     */
-    public static AppController getGlobalApplicationContext() {
-        if (instance == null)
-            throw new IllegalStateException("this application does not inherit com.kakao.GlobalApplication");
-        return instance;
+//        AppController.currentActivity = activity;
     }
 
     public static synchronized AppController getInstance() {
         return mInstance;
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        MultiDex.install(this);
+        super.attachBaseContext(base);
     }
 
     @Override
@@ -84,19 +60,6 @@ public class AppController extends Application {
 
         super.onCreate();
         mInstance = this;
-
-        /**
-         * 카카오톡
-         * 이미지 로더, 이미지 캐시, 요청 큐를 초기화한다.
-         */
-        instance = this;
-
-        // Session manager
-        session = new SessionManager(getApplicationContext());
-
-        Log.d(TAG, "카카오톡 시작전");
-        KakaoSDK.init(new KakaoSDKAdapter());
-        Log.d(TAG, "AppController시작");
     }
 
     /**
