cd2a03997d078dca5ed206c66e5cb95ac52dfd1f
ytkim4558 <ytkim4558@naver.com>
2016-11-15 19:06:09 +0900

    계속 확인중

--- app/src/main/java/com/nagnek/bikenavi/app/AppController.java
+++ app/src/main/java/com/nagnek/bikenavi/app/AppController.java
@@ -13,12 +13,25 @@ import com.android.volley.Cache;
 import com.android.volley.Network;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.BasicNetwork;
 import com.android.volley.toolbox.DiskBasedCache;
 import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.kakao.auth.KakaoSDK;
+import com.nagnek.bikenavi.helper.SQLiteHandler;
+import com.nagnek.bikenavi.helper.SessionManager;
 import com.nagnek.bikenavi.kakao.KakaoSDKAdapter;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.HashMap;
 
 /**
  * Created by yongtak on 2016-09-27.
@@ -34,7 +47,13 @@ public class AppController extends MultiDexApplication {
     public static final String TAG = AppController.class.getSimpleName();
     private static volatile AppController mInstance;
     private static volatile Activity currentActivity = null;
+    /**
+     * 크래쉬 방지용 코드 추가
+     * 참조 : http://www.kmshack.kr/2013/03/uncaughtexceptionhandler%EB%A5%BC-%EC%9D%B4%EC%9A%A9%ED%95%9C-%EC%95%B1-%EB%B9%84%EC%A0%95%EC%83%81-%EC%A2%85%EB%A3%8C%EC%8B%9C-log%EC%A0%84%EC%86%A1-%EB%B0%8F-%EC%9E%AC%EC%8B%A4%ED%96%89-%ED%95%98/
+     */
+    private Thread.UncaughtExceptionHandler mUncaughtExceptionhandler;
     private RequestQueue mRequestQueue;
+    private SessionManager session; // 로그인했는지 확인용 변수
 
     /**
      * 카카오톡
@@ -72,10 +91,126 @@ public class AppController extends MultiDexApplication {
          * 참조 : http://www.kmshack.kr/2013/03/uncaughtexceptionhandler%EB%A5%BC-%EC%9D%B4%EC%9A%A9%ED%95%9C-%EC%95%B1-%EB%B9%84%EC%A0%95%EC%83%81-%EC%A2%85%EB%A3%8C%EC%8B%9C-log%EC%A0%84%EC%86%A1-%EB%B0%8F-%EC%9E%AC%EC%8B%A4%ED%96%89-%ED%95%98/
          */
         Log.d(TAG, "onCreate시작");
+        mUncaughtExceptionhandler = Thread.getDefaultUncaughtExceptionHandler();
+        Thread.setDefaultUncaughtExceptionHandler(new uncaughtExceptionHandlerApplication());
 
         super.onCreate();
         mInstance = this;
+        // Session manager
+        session = new SessionManager(getApplicationContext());
         KakaoSDK.init(new KakaoSDKAdapter());
+        final RequestQueue requestQueue = Volley.newRequestQueue(this);
+    }
+
+    /**
+     * 크래쉬 방지용 코드 추가
+     * 참조 : http://www.kmshack.kr/2013/03/uncaughtexceptionhandler%EB%A5%BC-%EC%9D%B4%EC%9A%A9%ED%95%9C-%EC%95%B1-%EB%B9%84%EC%A0%95%EC%83%81-%EC%A2%85%EB%A3%8C%EC%8B%9C-log%EC%A0%84%EC%86%A1-%EB%B0%8F-%EC%9E%AC%EC%8B%A4%ED%96%89-%ED%95%98/
+     * 메시지로 변환
+     */
+    private String getStackTrace(Throwable th) {
+        final Writer result = new StringWriter();
+        final PrintWriter printWriter = new PrintWriter(result);
+
+        Throwable cause = th;
+        while (cause != null) {
+            cause.printStackTrace(printWriter);
+            cause = cause.getCause();
+        }
+        final String stacktraceAsString = result.toString();
+        printWriter.close();
+
+        return stacktraceAsString;
+    }
+
+    // 에러 전송
+    private void sendErrorReportToServer(final String errorMessage) {
+
+        // Tag used to cancel the request
+        String tag_string_req = "send_error_report.";
+
+        //JsonArrayRequest of volley
+        final StringRequest strReq = new StringRequest(Request.Method.POST, AppConfig.URL_USER_ERROR_REPORT,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        //Calling method parsePOIList to parse the json response
+                        try {
+                            Log.d(TAG, "response : " + response);
+                            JSONObject jsonObject = new JSONObject(response);
+                            boolean error = jsonObject.getBoolean("error");
+
+                            if (!error) {
+                                Log.d(TAG, "에러전송완료");
+                            } else {
+                                // Error in login. Get the error message
+                                String errorMsg = jsonObject.getString("error_msg");
+                                Log.d(TAG, errorMsg);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Log.d(TAG, e.toString());
+                        }
+                    }
+                },
+
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //If an error occurs that means end of the list has reached
+                        Log.d(TAG, error.toString());
+                    }
+                }) {
+            @Override
+            protected HashMap<String, String> getParams() {
+                HashMap<String, String> mRequestParams = new HashMap<String, String>();
+                if (session.isSessionLoggedIn()) {
+                    mRequestParams = inputUserInfoToInputParams(mRequestParams);
+                }
+                mRequestParams.put("MESSAGE", errorMessage);
+
+                return mRequestParams;
+            }
+        };
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(strReq, tag_string_req);
+    }
+
+    private HashMap<String, String> inputUserInfoToInputParams(HashMap<String, String> params) {
+        SessionManager session; // 로그인했는지 확인용 변수
+        SQLiteHandler db;   // sqlite
+        // SqLite database handler 초기화
+        db = SQLiteHandler.getInstance(this);
+        // Session manager
+        session = new SessionManager(this);
+        SQLiteHandler.UserType loginUserType = session.getUserType();
+        HashMap<String, String> user = db.getLoginedUserDetails(loginUserType);
+
+        switch (loginUserType) {
+            case BIKENAVI:
+                String email = user.get(SQLiteHandler.KEY_EMAIL);
+                params.put("email", email);
+                Log.d(TAG, "bikenavi타입 유저네" + email);
+                break;
+            case GOOGLE:
+                String googleemail = user.get(SQLiteHandler.KEY_GOOGLE_EMAIL);
+                params.put("googleemail", googleemail);
+                Log.d(TAG, "구글 유저네" + googleemail);
+                break;
+            case KAKAO:
+                String kakaoId = user.get(SQLiteHandler.KEY_KAKAO_ID);
+                params.put("kakaoid", kakaoId);
+                Log.d(TAG, "카카오 유저네" + kakaoId);
+                break;
+            case FACEBOOK:
+                String facebookId = user.get(SQLiteHandler.KEY_FACEBOOK_ID);
+                params.put("facebookid", facebookId);
+                Log.d(TAG, "페북 유저네" + facebookId);
+                break;
+        }
+
+        return params;
+
     }
 
     /**
@@ -113,6 +248,22 @@ public class AppController extends MultiDexApplication {
     public <T> void addToRequestQueue(Request<T> req) {
         req.setTag(TAG);
         getRequestQueue().add(req);
+    }
+
+    /**
+     * 크래쉬 방지용 코드 추가
+     * 참조 : http://www.kmshack.kr/2013/03/uncaughtexceptionhandler%EB%A5%BC-%EC%9D%B4%EC%9A%A9%ED%95%9C-%EC%95%B1-%EB%B9%84%EC%A0%95%EC%83%81-%EC%A2%85%EB%A3%8C%EC%8B%9C-log%EC%A0%84%EC%86%A1-%EB%B0%8F-%EC%9E%AC%EC%8B%A4%ED%96%89-%ED%95%98/
+     */
+    class uncaughtExceptionHandlerApplication implements Thread.UncaughtExceptionHandler {
+        @Override
+        public void uncaughtException(Thread thread, Throwable ex) {
+            // 예외상황이 발행 되는 경우 작업
+            Log.e("Error", getStackTrace(ex));
+            sendErrorReportToServer(getStackTrace(ex));
+
+            // 예외처리를 하지 않고 DefaultUncaughtException으로 넘긴다.
+            mUncaughtExceptionhandler.uncaughtException(thread, ex);
+        }
     }
 }
 
